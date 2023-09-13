e6d3a9821b88a3a36a29578100ed62758529ca2f
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-09-13 11:00:09 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let left = 0
+  let right = word.length-1
+  while(left<right){
+    if(word[left] != word[right]){
+      return false
+    }
+    left++
+    right--
+  }
+  return true
 }
 
+
 /* 
-  Add your pseudocode here
+left = 0
+right = length(word)-1
+
+while left < right do
+left++
+right--
+
+if word[left] !== word[right] then
+return false
+end
+
+return true
+
 */
 
+
 /*
-  Add written explanation of your solution here
+  my code assumes only letters or numbers will be passed in
+  i set one counter for the left letter and one for the right
+  run a while loop that runs until the left counter is more that the right counter
+  this makes it so the two counters point to letters and they stop in the middle so as not to be redundant
+  if the two pointers dont equal eachother then return false because its not a palindrome
+  at the end return true because if it didnt ever return false then its true
 */
 
 // You can run `node index.js` to view these console logs
